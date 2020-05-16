a3f0a95a71160e8500ad2b412096a8957643d2bb
xDarKFeniXx <dark.fenix.tula@gmail.com>
2020-05-17 01:47:57 +0300

    somplete HW3

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function start() {
         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
 }
-
+start();
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -30,18 +30,21 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
+rememberMyFilms();
 
 if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
@@ -53,4 +56,18 @@ if (personalMovieDB.count < 10) {
     console.log("Произошла ошибка");
 }
 
-console.log(personalMovieDB);
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
