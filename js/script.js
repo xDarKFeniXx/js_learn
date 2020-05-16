2978cc1a05f31f7b69bd16defd014c575038ff8c
xDarKFeniXx <dark.fenix.tula@gmail.com>
2020-05-16 20:20:00 +0300

    fist HW

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+const numberOfFilms=+prompt('Сколько фильмов вы уже посмотрели?', '')
+const personalMovieDB={
+    count: numberOfFilms,
+    movies: {},
+    actors:{},
+    genres:[],
+    privat:false
+};
+const a=promt('один из последнийх просмотренных фильмов', ''),
+      b=promt('На сколько вы его оцените', ''),
+      c=promt('один из последнийх просмотренных фильмов', ''),
+      d=promt('На сколько вы его оцените', '');
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+console.log(personalMovieDB);
