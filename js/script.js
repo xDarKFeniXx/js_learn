6fc00204dca2572dd471c9d7376ad35e4717b343
xDarKFeniXx <dark.fenix.tula@gmail.com>
2020-05-16 23:38:17 +0300

    complete HW2

--- js/script.js
+++ js/script.js
@@ -12,35 +12,39 @@ str.length - и получить её длину)
 "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
 4) Потренироваться и переписать цикл еще двумя способами*/
-"use strict";
-const numberOfFilms=+prompt('Сколько фильмов вы уже посмотрели?', '')
-const personalMovieDB={
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
-    actors:{},
-    genres:[],
-    privat:false
+    actors: {},
+    genres: [],
+    privat: false
 };
-const a=promt('один из последнийх просмотренных фильмов', ''),
-      b=promt('На сколько вы его оцените', ''),
-      c=promt('один из последнийх просмотренных фильмов', ''),
-      d=promt('На сколько вы его оцените', '');
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-
-
-
-
-
-if (personalMovieDB.count>30){
-    console.log('Вы киноман');    
-} else if (personalMovieDB.count>=10){
-    console.log('Вы классический зритель');
-} else if(personalMovieDB.count<10){
-    console.log('Просмотрено довольно мало фильмов');
-} else {
-    console.log('Произошла ошибка');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 }
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
 console.log(personalMovieDB);
