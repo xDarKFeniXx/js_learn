edbd1105995f6675ddfebc2ae81dbe06578229cb
xDarKFeniXx <dark.fenix.tula@gmail.com>
2020-05-19 17:29:16 +0300

    add default className for MenuCard class

--- js/script.js
+++ js/script.js
@@ -131,12 +131,13 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //cadrs class
     class MenuCard{
-        constructor(src, alt, title, descr, price, parentSelector){
+        constructor(src, alt, title, descr, price, parentSelector, ...classes){
             this.src=src;
             this.alt=alt;
             this.title=title;
             this.descr=descr;
             this.price=price;
+            this.classes=classes;
             this.parent=document.querySelector(parentSelector);
             this.transfer=75;
             this.changeToUAH();
@@ -146,8 +147,15 @@ window.addEventListener('DOMContentLoaded', function () {
         }
         render(){
             const element=document.createElement('div');
+            if (this.classes.length==0){
+                this.element='menu__item';
+                element.classList.add(this.element);
+            }else{
+                this.classes.forEach(className=>element.classList.add(className));
+            }
+            
             element.innerHTML=`
-            <div class="menu__item">
+            
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
@@ -156,7 +164,7 @@ window.addEventListener('DOMContentLoaded', function () {
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> р/день</div>
                     </div>
-                </div>
+              
                 `;
                 this.parent.append(element);
         }
@@ -167,7 +175,8 @@ window.addEventListener('DOMContentLoaded', function () {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        ".menu .container"
+        ".menu .container",
+        'menu__item',
     ).render();
 
     new MenuCard(
@@ -176,7 +185,8 @@ window.addEventListener('DOMContentLoaded', function () {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         14,
-        ".menu .container"
+        ".menu .container",
+        'menu__item',
     ).render();
 
     new MenuCard(
@@ -185,6 +195,7 @@ window.addEventListener('DOMContentLoaded', function () {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         21,
-        ".menu .container"
+        ".menu .container",
+        'menu__item'
     ).render();
 });
