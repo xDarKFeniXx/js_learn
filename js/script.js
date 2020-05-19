4043d32cd61f10eed96dcee2d946d42ae86a459f
xDarKFeniXx <dark.fenix.tula@gmail.com>
2020-05-19 20:14:43 +0300

    add server.php and POST form HW13

--- js/script.js
+++ js/script.js
@@ -101,7 +101,7 @@ window.addEventListener('DOMContentLoaded', function () {
         modal.classList.remove('hide');
         // modal.classList.toggle('show');
         document.body.style.overflow = 'hidden';
-        clearInterval(ModalTimeId);
+        // clearInterval(ModalTimeId);
     }
     modalTrigger.forEach(modalWindow => {
         modalWindow.addEventListener('click', openModal)
@@ -115,7 +115,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     })
     document.addEventListener('keydown', (e) => {
-        if (e.code = 'Escape' && modal.classList.contains('show')) {
+        if (e.code === "Escape" && modal.classList.contains('show')) { 
             closeModal();
         }
     });
@@ -198,4 +198,53 @@ window.addEventListener('DOMContentLoaded', function () {
         ".menu .container",
         'menu__item'
     ).render();
+
+    //forms
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.appendChild(statusMessage);
+        
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
